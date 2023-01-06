8e9a4234c6d753f1671c25f378b0179e1a6a3713
WooYoung <wymoon2690@gmail.com>
2023-01-07 03:57:04 +0900

    swift

--- swift/Sources/swift/swift.swift
+++ swift/Sources/swift/swift.swift
@@ -1,8 +1,23 @@
+import Foundation
+
 @main
 public struct swift {
     public private(set) var text = "Hello, World!"
 
     public static func main() {
         print(swift().text)
+
+        var add: Int64  = 0
+        let st = DispatchTime.now()
+        for index: Int64 in 1...100000000 {
+            add = add + index
+        }
+        let ed = DispatchTime.now()
+        let nanoTime = ed.uptimeNanoseconds - st.uptimeNanoseconds
+
+        print("==========================")
+        print("swift")
+        print(add)
+        print(Double(nanoTime) / 1_000_000_000 )
     }
 }
